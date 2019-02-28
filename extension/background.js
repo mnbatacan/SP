19052339cdd480e8820b03a0a10a49530f2ef739
maureen <mnbatacan@up.edu.ph>
2019-02-28 09:06:35 +0800

    gets the authToken from chrome.identity

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,7 @@
   chrome.browserAction.onClicked.addListener(function() {
     chrome.tabs.create({url: 'index1.html'});
   });
+
+
+
+  console.log("yow")
--- extension/index.js
+++ extension/index.js
@@ -1,29 +1,52 @@
-$.ajax({
-    url: 'https://apis.google.com/js/api.js?onload=handleClientLoad()',
-    dataType: 'script',
-    async: true,
-    defer: true
-});
+// ajax({
+//     url: 'https://apis.google.com/js/api.js',
+//     dataType: 'script',
+//     async: true,
+//     defer: true
+// });
 
 
-// "https://apis.google.com/js/api.js"
-//       onload="this.onload=function(){};handleClientLoad()"
-//       onreadystatechange="if (this.readyState === 'complete') this.onload()
+// // "https://apis.google.com/js/api.js"
+// //       onload="this.onload=function(){};handleClientLoad()"
+// //       onreadystatechange="if (this.readyState === 'complete') this.onload()
 
 
- $(document).ready(function(){
-    $('.tabs').tabs();
-    $.getScript("https://apis.google.com/js/api.js?onload=handleClientLoad()", function(){
-    	if (this.readyState === 'complete')
-	  // alert("Script loaded and executed.");
-	console.log('foo');
-		handleClientLoad();
-	});
+//  $(document).ready(function(){
+//     $('.tabs').tabs();
+//     $.getScript("https://apis.google.com/js/api.js?onload=handleClientLoad", function(){
+//     	// if (this.readyState === 'complete')
+// 	  // alert("Script loaded and executed.");
+
+// 	});
 	  
-  });
+//   });
 
 
+ var head = document.getElementsByTagName('head')[0];
+ var script = document.createElement('script');
+ script.type = 'text/javascript';
+ script.src = "https://apis.google.com/js/api.js?onload=handleClientLoad";
+script.async= true;
+script.defer= true;
 
+ script.onreadystatechange = function () {
+  if (this.readyState === 'complete' || this.readyState === 'loaded') {
+    handleClientLoad();
+  }
+};
+
+window.onload = function() {
+ // alert("let's go!");
+
+
+ head.appendChild(script);
+ // script.onload = handleClientLoad();
+
+// chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
+//   // Use the token.
+//   console.log("asdasda");
+//   console.log(token);
+// });
 
 
 
@@ -44,23 +67,28 @@ const channelForm = document.getElementById('channel-form');
 // const content = document.getElementById('content');
 const videoContainer = document.getElementById('video-container');
 
-
-
-function handleClientLoad() {
-    gapi.load('client:auth2', initClient());
-
 }
+
+// function handleClientLoad() {
+// 	console.log("bwifebise")
+//     gapi.load('client:auth2', function(){initClient();});
+
+// 	console.log("handleClientLoad: done");
+	
+// }
 
 
 function initClient() {
-	// alert("Script loaded and executed.");
+	console.log("initClient: ---");
+
 	gapi.client.init({
 	    discoveryDocs: DISCOVERY_DOCS,
     	clientId: CLIENT_ID,
       	scope: SCOPES
     }).then(function () {
     // Listen for sign-in state changes.
-
+    	console.log("initClient: then");
+	
     	gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus());
 
         // Handle the initial sign-in state.
@@ -69,6 +97,39 @@ function initClient() {
         signoutButton.onclick = handleSignoutClick;
         });
       }
+
+
+
+function handleClientLoad(){
+  console.log("handleClientLoad: done");
+  gapi.load('client', {
+    callback: function() {
+      // Handle gapi.client initialization.
+      chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
+        // Use the token.
+        console.log("GOT IN");
+        console.log(token);
+        gapi.client.setToken({
+          access_token: token
+        });
+      });
+      
+    },
+    onerror: function() {
+      // Handle loading error.
+      alert('gapi.client failed to load!');
+    },
+    timeout: 5000, // 5 seconds.
+    ontimeout: function() {
+      // Handle timeout.
+      alert('gapi.client could not load in a timely manner!');
+    }
+  });
+}
+
+
+
+
 
 
 function updateSigninStatus(isSignedIn) {
